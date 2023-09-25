5bbbda46418dc08ed3dc695a69f82ae02bab1d02
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-25 14:36:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_ht
 with st.sidebar:
     tabs = on_hover_tabs(tabName=['Home', 'EDA','Topic Analysis','Subtopics', 'Likes','Dislikes','Sentiment','Competition'], 
                          iconName=['🏠', '📊','💡', '📝','👍','👎','💭','🏆'], 
-                         styles = {'navtab': {'background-color':'#9FACB8',
+                         styles = {'navtab': {'background-color':'#094780',
                                                   'color': '#08',
                                                   'font-size': '17px',
                                                   'transition': '.3s',
