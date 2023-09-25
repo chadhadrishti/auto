905796f638c15e56c5c123133d855a295e4e900a
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-25 12:53:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
 
 
 if tabs =='Home':
-        st.write(f'<h1 style="color:#EC2A39;font-size:15px;">{"*Select the Brand for which you would like to see the Report :*"}</h1>', unsafe_allow_html=True)
+        st.write(f'<h1 style="text-align: center;color:#9FACB8;font-size:15px;">{"Select the Brand for which you would like to see the Report :"}</h1>', unsafe_allow_html=True)
         st.write('Suzuki/Honda/TVS'.format(tabs))
 
 elif tabs == 'EDA':
