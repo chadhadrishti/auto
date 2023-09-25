ce0789203908da595bb322716447acba363524cc
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-25 13:43:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+import streamlit_antd_components as sac
 st.set_page_config(layout="wide")
 st.header("Automotive Customer Review Analysis")
 # st.write("Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success.")
 st.write(f'<h1 style="color:#EC2A39;font-size:15px;">{"Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success."}</h1>', unsafe_allow_html=True)
 st.markdown("""<hr style="height:1px;border:none;color:#9FACB8;background-color:#9FACB8;" /> """, unsafe_allow_html=True)
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+sac.divider(label='divider', icon='house', align='center')
 
 with st.sidebar:
     tabs = on_hover_tabs(tabName=['Home', 'EDA','Topic Analysis','Subtopics', 'Likes','Dislikes','Sentiment','Competition'], 
                          iconName=['🏠', '📊','💡', '📝','👍','👎','💭','🏆'], 
-                         styles = {'navtab': {'background-color':'#094780',
-                                                  'color': '#094780',
+                         styles = {'navtab': {'background-color':'#111',
+                                                  'color': '#08',
                                                   'font-size': '16px',
                                                   'transition': '.3s',
                                                   'white-space': 'nowrap',
