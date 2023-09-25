065316617c3839c216d5fd682c4f935e9d1e162e
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-25 14:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit_antd_components as sac
 st.set_page_config(layout="wide")
 st.header("Automotive Customer Review Analysis")
 # st.write("Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success.")
-st.write(f'<h1 style="color:#EC2A39;font-size:15px;">{"Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success."}</h1>', unsafe_allow_html=True)
+st.write(f'<h1 style="margi-top:-20px,color:#EC2A39;font-size:15px;">{"Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success."}</h1>', unsafe_allow_html=True)
 # st.markdown("""<hr style="height:1px;border:none;color:#9FACB8;background-color:#9FACB8;" /> """, unsafe_allow_html=True)
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 sac.divider(label='🏠', align='center')
@@ -30,14 +30,17 @@ with st.sidebar:
 
 
 if tabs =='Home':
-        st.write(f'<h1 style="text-align: center;color:#094780;font-size:15px;">{"_Key Take Aways_ : Topics being discussed | Likes in the SKUs | Dislikes in the SKUs | Customer Sentiment | Competitive Analysis | Major Keywords | Subtopics across different automotive Key Factors"}</h1>', unsafe_allow_html=True)
+        st.write(f'<h1 style="text-align: center;color:#094780;font-size:15px;">{"Key Take Aways : Topics being discussed | Likes in the SKUs | Dislikes in the SKUs | Customer Sentiment | Competitive Analysis | Major Keywords | Subtopics across different automotive Key Factors"}</h1>', unsafe_allow_html=True)
         st.write(f'<h1 style="text-align: center;color:#9FACB8;font-size:15px;">{"Select the Brand for which you would like to see the Report :"}</h1>', unsafe_allow_html=True)
         st.image('Picture1.png', use_column_width=True)
+        col11, col22, col33 = st.columns(3)
+        
     # st.write('Suzuki/Honda/TVS'.format(tabs))
 
 elif tabs == 'EDA':
         st.title("EDA")
         st.write('Add wcloud and some charts'.format(tabs))
+    
 
 elif tabs == 'Topic Analysis':
         st.title("Topics")
