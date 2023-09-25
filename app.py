97261b72abea86ece2ffa0a70f52ee768e875154
chadhadrishti <98087720+chadhadrishti@users.noreply.github.com>
2023-09-25 14:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit_antd_components as sac
 st.set_page_config(layout="wide")
 st.header("Automotive Customer Review Analysis")
 # st.write("Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success.")
-st.write(f'<h1 style="margi-top:-20px,color:#EC2A39;font-size:15px;">{"Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success."}</h1>', unsafe_allow_html=True)
+st.write(f'<h1 style="margin-top:-20px;color:#EC2A39;font-size:15px;">{"Turn every review into a pit stop for improvement with our Automotive Review Analysis App – where user feedback fuels your success."}</h1>', unsafe_allow_html=True)
 # st.markdown("""<hr style="height:1px;border:none;color:#9FACB8;background-color:#9FACB8;" /> """, unsafe_allow_html=True)
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 sac.divider(label='🏠', align='center')
